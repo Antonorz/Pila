c7703923ccdebe4c08ac5465fda95fd403146847
Antonorz <131824060+Antonorz@users.noreply.github.com>
2023-09-11 21:05:49 -0600

    Create Nodo.java
    Creación de atributos privados un entero llamado dato y un Nodo llamado nodo, y u constructor que inicialice a dato y a siguiente como nulo.

---
+++ Nodo.java
@@ -0,0 +1,15 @@
+package ___;
+
+/**
+ *
+ * @author Anton
+ */
+
+public class Nodo {
+    private Integer dato;
+    private Nodo nodo;
+    
+    public Nodo() {
+        this.dato = null;
+        this.nodo = null;
+    }
