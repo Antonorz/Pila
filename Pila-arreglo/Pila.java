28ffefb27f0d656d334ae74f51e546f99060d123
Antonorz <131824060+Antonorz@users.noreply.github.com>
2023-09-11 22:08:22 -0600

    Update Pila.java
    Comentarios.

--- Pila-arreglo/Pila.java
+++ Pila-arreglo/Pila.java
@@ -8,7 +8,7 @@ public class Pila {
 
     private Nodo cima;
     private int tamanio;
-
+    //Insertar un elemento en la cima.
     public void insertar(int dato) {
         Nodo nuevoNodo = new Nodo();
         nuevoNodo.setDato(dato);
@@ -20,27 +20,27 @@ public class Pila {
         }
         tamanio++;
     }
-
+    //Quitar el elemento de la cima.
     public int quitar() {
         int datoCima = cima.getDato();
         cima = cima.getNodo();
         tamanio--;
         return datoCima;
     }
-
+    //Comprobar si una pila está vacía
     public boolean estaVacia() {
         return cima == null;
     }
-
+    //Vaciar pila
     public void vaciar() {
         cima = null;
         tamanio = 0;
     }
-    
+    //Obtener el elemento que está en la cima de la pila
     public int obtenerCima() {
         return cima.getDato();
     }
-    
+    //Obtener el tamaño de la pila
     public int getTamanio() {
         return tamanio;
     }
