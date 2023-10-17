3613e327cce7bdbaacc52d8be8ae46294409de0a
Antonorz <131824060+Antonorz@users.noreply.github.com>
2023-10-17 13:38:25 -0600

    Update QuickSort.java

--- Ordenamiento/QuickSort.java
+++ Ordenamiento/QuickSort.java
@@ -1,6 +1,6 @@
 public class QuickSort {
     public static int quickSort(int arr[]){
-      return arr;
+        return quickSort(arr, 0 y la longitud de arr – 1.);
     }
 
     private static int quickSort(int arr[], int primero, int ultimo){
