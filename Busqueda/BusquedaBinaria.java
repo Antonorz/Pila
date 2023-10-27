a5817d6bc3d4293915069545100f35c2e41806a2
Antonorz <131824060+Antonorz@users.noreply.github.com>
2023-10-27 10:52:28 -0600

    Update BusquedaBinaria.java
    Pendiente de cambios.

--- Busqueda/BusquedaBinaria.java
+++ Busqueda/BusquedaBinaria.java
@@ -1,1 +1,21 @@
-
+public class BusquedaBinaria{
+    public static int buscador(int[] arr, int busqueda){
+        int inicio = 0; 
+        int fin = arr.length - 1;
+        while (inicio < fin) {
+            int centro = ((inicio + fin) / 2 );
+            if (arr[centro] == busqueda){
+                return centro;
+            }
+                
+            if (arr[centro] < busqueda){
+                //System.out.println(centro); 
+                inicio = centro;
+            } else {
+                //System.out.println(centro); 
+                fin = centro;    
+            }
+        }
+        return -1;
+    }
+}
