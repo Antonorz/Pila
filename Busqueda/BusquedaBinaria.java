f9d90495c8413efc2b5014df6b306c8b74a4714b
Antonorz <131824060+Antonorz@users.noreply.github.com>
2023-10-27 21:27:12 -0600

    Update BusquedaBinaria.java

--- Busqueda/BusquedaBinaria.java
+++ Busqueda/BusquedaBinaria.java
@@ -1,21 +1,45 @@
-public class BusquedaBinaria{
-    public static int buscador(int[] arr, int busqueda){
-        int inicio = 0; 
+import java.util.Random;
+
+public class BusquedaBinaria {
+    public static int buscador(int[] arr, int busqueda) {
+        int inicio = 0;
         int fin = arr.length - 1;
-        while (inicio < fin) {
-            int centro = ((inicio + fin) / 2 );
-            if (arr[centro] == busqueda){
+        
+        while (inicio <= fin) {
+            int centro = (inicio + fin + 1) / 2;
+            
+            if (arr[centro] == busqueda) {
                 return centro;
-            }
-                
-            if (arr[centro] < busqueda){
-                //System.out.println(centro); 
-                inicio = centro;
+            } else if (arr[centro] < busqueda) {
+                inicio = centro + 1;
             } else {
-                //System.out.println(centro); 
-                fin = centro;    
+                fin = centro - 1;
             }
         }
         return -1;
+    }
+    
+    public static int[] arrayAleatorio() {
+        Random aleatorio = new Random();
+        int[] arr = new int[1000];
+        int index = 0;
+    
+        while (index < 1000) {
+            int nuevoNumero = aleatorio.nextInt(6562) + 1; //Matricula
+            boolean repetido = false;
+    
+            for (int i = 0; i < index; i++) {
+                if (arr[i] == nuevoNumero) {
+                    repetido = true;
+                    break;
+                }
+            }
+    
+            if (!repetido) {
+                arr[index] = nuevoNumero;
+                index++;
+            }
+        }
+        return arr;
     }
 }
