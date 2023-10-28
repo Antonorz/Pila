da0400f9b6f3e6d9aa261ab3ef3e053ba06eff6e
Antonorz <131824060+Antonorz@users.noreply.github.com>
2023-10-27 21:28:12 -0600

    Update Main.java

--- Busqueda/Main.java
+++ Busqueda/Main.java
@@ -4,14 +4,27 @@ import java.util.Arrays;
 public class Main{
     public static void main (String[] args) {
         Scanner escaner = new Scanner(System.in);
-        int[] arr = {1, 2, 3, 4, 5, 6};
+        
+        int[] arr = BusquedaBinaria.arrayAleatorio();
+        
+        System.out.println("Arreglo generado.");
+        System.out.println("Antes: " + Arrays.toString(arr));
+        System.out.println();
+        
+        int[] arrMergeSort = MergeSort.mergeSort(arr);
+        
+        System.out.println("Ahora: " + Arrays.toString(arrMergeSort));
+        System.out.println();
+        
         System.out.print("Inserte el entero que desee buscar: ");
+        
         int busqueda = escaner.nextInt();
-        int posicion = BusquedaBinaria.buscador(arr, busqueda);
+        int posicion = BusquedaBinaria.buscador(arrMergeSort, busqueda);
+        
         if(posicion == -1){
-            System.out.print("El entero que desea buscar no se encuentra dentro del array.");
+            System.out.println("El entero que desea buscar no se encuentra dentro del array.");
         }else{
-            System.out.println("El entero que desea buscar se ubica en: " + posicion);    
+            System.out.println("El entero que desea buscar se ubica en la posicion: " + posicion);    
         }
     }
 }
