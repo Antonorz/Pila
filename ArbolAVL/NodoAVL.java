cd06cee944a22dd2ea7a48397d16881f29930c96
Antonorz <131824060+Antonorz@users.noreply.github.com>
2023-11-17 20:59:27 -0600

    Create NodoAVL.java

---
+++ ArbolAVL/NodoAVL.java
@@ -0,0 +1,11 @@
+public class NodoAVL {
+    int altura;
+    int dato;
+    NodoAVL izquierda;
+    NodoAVL derecha;
+
+    NodoAVL(int dato) {
+        this.dato = dato;
+        this.altura = 1;
+    }
+}
