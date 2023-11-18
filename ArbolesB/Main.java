192a77c71000aa531bcb9aa30dbb6b14997f07c2
Antonorz <131824060+Antonorz@users.noreply.github.com>
2023-11-17 20:03:18 -0600

    Update Main.java (comentarios)

--- ArbolesB/Main.java
+++ ArbolesB/Main.java
@@ -10,12 +10,12 @@ public class Main {
         arbolB.insertar(arbolB, 2);
         arbolB.insertar(arbolB, 5);
 
-        // Imprimir el arbol B con nodos insertados
+        //Imprimir el arbol B con nodos insertados
         System.out.println("Arbol B con nodos insertados:");
         arbolB.imprimir(arbolB.raiz);
         System.out.println();
 
-        // Buscar nodos
+        //Buscar nodos
         System.out.println("Buscando nodo (10):");
         arbolB.imprimirNodoEspecifico(arbolB, 10);
         System.out.println();
@@ -25,7 +25,7 @@ public class Main {
         System.out.println();
 
 
-        // Eliminar una llave
+        //Eliminar una llave
         System.out.println("Arbol B antes de eliminar la llave 1:");
         arbolB.imprimir(arbolB.raiz);
         System.out.println("");
@@ -41,7 +41,7 @@ public class Main {
         arbolB.insertar(arbolB, 8);
 
 
-        // Imprimir el arbol B con nodos insertados
+        //Imprimir el arbol B con nodos insertados
         System.out.println("Arbol B con nodos insertados:");
         arbolB.imprimir(arbolB.raiz);
         System.out.println();
@@ -51,12 +51,12 @@ public class Main {
         arbolB.insertar(arbolB, 13);
         arbolB.insertar(arbolB, 14);
         
-        // Imprimir el arbol B con nodos insertados
+        //Imprimir el arbol B con nodos insertados
         System.out.println("Arbol B con MAS nodos insertados:");
         arbolB.imprimir(arbolB.raiz);
         System.out.println();
         
-        // Buscar nodos
+        //Buscar nodos
         System.out.println("Buscando nodo (8):");
         arbolB.imprimirNodoEspecifico(arbolB, 8);
         System.out.println();
